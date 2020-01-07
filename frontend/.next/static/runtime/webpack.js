067a255f66b3e4a64bbc5a39b81a79746229ea33
Dusty <dustygaron@hotmail.com>
2020-01-07 14:18:23 -0500

    Added User signup

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3ef3ee0e893ce8a6bc3b";
+/******/ 	var hotCurrentHash = "270e8355f57105a294e2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ frontend/components/Signup.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+import Form from './styles/Form'
+import Error from './ErrorMessage'
+
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION($email: String!, $name: String!, $password: String!) {
+    signup(email: $email, name: $name, password: $password){
+      id
+      email
+      name
+    }
+  }
+`
+
+class Signup extends React.Component {
+
+  state = {
+    email: '',
+    name: '',
+    password: ''
+  }
+
+  saveToState = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <Mutation mutation={SIGNUP_MUTATION} variables={this.state}>
+        {(signup, { error, loading }) => {
+
+          return (
+            <Form method='POST'
+              onSubmit={async e => {
+                e.preventDefault()
+                const res = await signup()
+                // console.log(res)
+                this.setState({
+                  email: '',
+                  name: '',
+                  password: ''
+                })
+              }}>
+              <fieldset disabled={loading} aria-busy={loading}>
+                <h2>Sign up for an account</h2>
+                <Error error={error} />
+                <label htmlFor='email'>
+                  Email
+          <input
+                    type='text'
+                    name='email'
+                    placeholder='Email'
+                    value={this.state.email}
+                    onChange={this.saveToState}
+                  />
+                </label>
+                <label htmlFor='name'>
+                  Name
+          <input
+                    type='text'
+                    name='name'
+                    placeholder='Name'
+                    value={this.state.name}
+                    onChange={this.saveToState}
+                  />
+                </label>
+                <label htmlFor='password'>
+                  Password
+          <input
+                    type='text'
+                    name='password'
+                    placeholder='Password'
+                    value={this.state.password}
+                    onChange={this.saveToState}
+                  />
+                </label>
+                <button type='submit'>Sign Up</button>
+              </fieldset>
+            </Form>)
+        }}
+      </Mutation>
+    )
+  }
+}
+
+export default Signup 
