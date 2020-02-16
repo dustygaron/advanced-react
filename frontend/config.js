c446b53e2f382c6ac96be2fc0877d6019debc91a
Dusty Garon <hello@dustygaron.com>
2020-02-15 19:55:46 -0500

    Frontend

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`
-export const prodEndpoint = `https://store-server-6fd3c75b33.herokuapp.com/store-server/prod`
+export const prodEndpoint = `https://dev-life-yoga-prod.herokuapp.com/`
 export const perPage = 4
